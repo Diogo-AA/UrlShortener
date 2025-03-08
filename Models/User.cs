947d2c7b6b429e5c5d6451edc8c92e3f75013f31
Diogo AA <diogomanu91@protonmail.com>
2025-03-08 15:51:54 +0100

    Added Equals and GetHashCode methods to User model to be able to compare Users models

--- Models/User.cs
+++ Models/User.cs
@@ -10,4 +10,18 @@ public class User
         Id = id;
         Username = username;
     }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is User user)
+        {
+            return this.Username == user.Username && this.Id == user.Id;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Username);
+    }
 }
