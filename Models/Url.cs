a1eb8a208f556461c77dbe0fb530221a66b9cba1
Diogo AA <diogomanu91@protonmail.com>
2025-03-08 15:49:02 +0100

    Changed Url model name and added method to verify if a uri si valid

--- Models/Url.cs
+++ Models/Url.cs
@@ -1,6 +1,6 @@
 ﻿namespace UrlShortener.Models;
 
-public class UrlShorted
+public class Url
 {
     public const string ALLOWED_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     public const int URL_SHORTED_LENGTH = 6;
@@ -9,4 +9,11 @@ public class UrlShorted
     public Guid UserId { get; set; }
     public required string ShortedUrl { get; set; }
     public required string OriginalUrl { get; set; }
+
+    public static bool IsValidUrl(string url)
+    {
+        Uri uriResult;
+        return Uri.TryCreate(url, UriKind.Absolute, out uriResult)
+            && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+    }
 }
